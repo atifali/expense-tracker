5a0c847815a96e90aca768b7d3c30dbac7360d71
Atif Ali <atifshoukatali@yahoo.com>
2025-09-10 19:31:20 -0700

    add input component

--- frontend/src/components/inputs/Input.jsx
+++ frontend/src/components/inputs/Input.jsx
@@ -1,6 +1,25 @@
-const Input = () => {
+import { useState } from "react"
+
+const Input = ({ value, onChange, placeholder, label, type }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
     return (
-        <div>Input</div>
+        <div>
+            <label className="text-[13px] text-slate-800">{label}</label>
+            <div className="input-box">
+                <input
+                    className="w-full bg-transparent outline-none"
+                    type={type == "password" ? showPassword ? "text" : "password" : type}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={(e) => onChange(e)}
+                />
+            </div>
+        </div>
     )
 }
 
