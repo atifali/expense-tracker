fa644f66c669d529d00c480236d15d4172b48b27
Atif Ali <atifshoukatali@yahoo.com>
2025-09-17 19:33:48 -0700

    add stateful vars to sign up

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react"
+import AuthLayout from "../../components/layouts/AuthLayout"
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../components/inputs/Input";
+import { validateEmail } from "../../utils/helper";
+
 const SignUp = () => {
+    const [profilePic, setProfilePic] = useState(null);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleSignUp = async (e) => { }
+
     return (
         <div>SignUp</div>
     )
