4fec85a715b3d2f4b72b506860cd142ec182e9e9
Atif Ali <atifshoukatali@yahoo.com>
2025-09-18 19:23:51 -0700

    add layout for signup form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Root />} />
             <Route path="/login" exact element={<Login />} />
-            <Route path="/sign-up" exact element={<SignUp />} />
+            <Route path="/signup" exact element={<SignUp />} />
             <Route path="/dashboard" exact element={<Home />} />
             <Route path="/income" exact element={<Income />} />
             <Route path="/expense" exact element={<Expense />} />
--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -16,7 +16,43 @@ const SignUp = () => {
     const handleSignUp = async (e) => { }
 
     return (
-        <div>SignUp</div>
+        <AuthLayout>
+            <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+                <h3 className="text-xl font-semibold text-black">
+                    Create an Account!
+                </h3>
+                <p className="text-xs text-slate-700 mt-[5px] mb-6">
+                    Join us today by entering your details below:
+                </p>
+                <form onSubmit={handleSignUp}>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <Input
+                            value={fullName}
+                            onChange={({ target }) => setFullName(target.value)}
+                            label="Full Name"
+                            placeholder="John"
+                            type="text"
+                        />
+                        <Input
+                            value={email}
+                            onChange={({ target }) => setEmail(target.value)}
+                            label="Email Address"
+                            placeholder="john@example.com"
+                            type="text"
+                        />
+                        <div className="col-span-2">
+                            <Input
+                                value={password}
+                                onChange={({ target }) => setPassword(target.value)}
+                                label="Password"
+                                placeholder="Min 8 Characters"
+                                type="password"
+                            />
+                        </div>
+                    </div>
+                </form>
+            </div >
+        </AuthLayout >
     )
 }
 
