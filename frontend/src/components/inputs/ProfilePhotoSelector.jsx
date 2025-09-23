e84d6746674e0e51b6905b55f468c692ab45fd7c
Atif Ali <atifshoukatali@yahoo.com>
2025-09-22 18:55:19 -0700

    add styling to profile photo selector

--- frontend/src/components/inputs/ProfilePhotoSelector.jsx
+++ frontend/src/components/inputs/ProfilePhotoSelector.jsx
@@ -33,26 +33,26 @@ const ProfilePhotoSelector = ({ image, setImage }) => {
                 className="hidden"
             />
             {!image ? (
-                <div className="">
-                    <LuUser className="" />
+                <div className="w-20 h-20 flex items-center justify-center bg-purple-100 rounded-full relative">
+                    <LuUser className="text-4xl text-primary" />
                     <button
                         type="button"
-                        className=""
+                        className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1"
                         onClick={onChooseFile}
                     >
                         <LuUpload />
                     </button>
                 </div>
             ) : (
-                <div className="">
+                <div className="relative">
                     <img
                         src={previewUrl}
                         alt="Profile Photo"
-                        className=""
+                        className="w-20 h-20 rounded-full object-cover"
                     />
                     <button
                         type="button"
-                        className=""
+                        className="w-8 h-8 flex items-center justify-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1"
                         onClick={handleRemoveImage}
                     >
                         <LuTrash />
