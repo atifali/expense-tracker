d7409a101adbddab011b7100b47246653c31ce20
Atif Ali <atifshoukatali@yahoo.com>
2025-09-23 18:55:12 -0700

    add value checks for signup

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -14,7 +14,23 @@ const SignUp = () => {
 
     const navigate = useNavigate();
 
-    const handleSignUp = async (e) => { }
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        let profileImageUrl = "";
+        if (!fullName) {
+            setError("Please enter your name.");
+            return;
+        }
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+        if (!password) {
+            setError("Please enter a password.");
+            return;
+        }
+        setError("");
+    }
 
     return (
         <AuthLayout>
