83fc1ace1dca48d8bf15be2cc3345e4fede3a22f
Atif Ali <atifshoukatali@yahoo.com>
2025-09-25 19:05:55 -0700

    add boilerplate to server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+
+    })
+);
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
