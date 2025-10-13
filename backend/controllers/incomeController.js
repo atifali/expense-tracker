e9a55cb3886a9d5731daa98119585b288b5b822b
Atif Ali <atifshoukatali@yahoo.com>
2025-10-12 21:38:35 -0700

    add individual income controller

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,7 +1,27 @@
 const User = require("../models/User");
 const Income = require("../models/Income");
 
-exports.addIncome = async (req, res) { }
-exports.getAllIncome = async (req, res) { }
-exports.deleteIncome = async (req, res) { }
-exports.downloadIncomeExcel = async (req, res) { }
+exports.addIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const { icon, source, amount, date } = req.body;
+        if (!source || !amount || !date) {
+            return res.status(400).json({ message: "All fields are required!" });
+        }
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        });
+        await newIncome.save();
+        return res.status(200).json(newIncome);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error!" });
+    }
+}
+exports.getAllIncome = async (req, res) => { }
+exports.deleteIncome = async (req, res) => { }
+exports.downloadIncomeExcel = async (req, res) => { }
